2c57ba0fc3449c13ca42fcaf422ace6ce5303c82
shahnwaz khan <88053176+shahnwaz123@users.noreply.github.com>
2021-08-12 16:59:22 +0530

    Add files via upload

---
+++ conditional_statement .py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # conditional statement(if,elif,else)
+
+# In[1]:
+
+
+# If,else and elif command used to run the code on condition basis.
+# Example-
+
+
+# In[8]:
+
+
+var1=41
+var2=56
+var3=int(input())
+if var3>var2:
+    print("greater")
+elif var3==var2:
+    print("equal")
+else:
+    print("lesser")
+
+
+# In[9]:
+
+
+var1=41
+var2=56
+var3=int(input())
+if var3>var2:
+    print("greater")
+elif var3==var2:
+    print("equal")
+else:
+    print("lesser")
+
+
+# In[10]:
+
+
+var1=41
+var2=56
+var3=int(input())
+if var3>var2:
+    print("greater")
+elif var3==var2:
+    print("equal")
+else:
+    print("lesser")
+
+
+# # Quiz
+
+# In[16]:
+
+
+# if age of person is greater than 18 then he can drive if less than 18 he cant drive,if equal to 18 we cant decide.
+
+
+# In[19]:
+
+
+age=int(input())
+if age>18:
+    print("you can drive man")
+elif age==18:
+    print('we cant decide man')
+else:
+    print("you cant drive man")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
